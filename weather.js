f35cbc9451285b526bad991e57964553d1ca9a39
LOVE KESH SHARMA <156709375+lovekesh7890@users.noreply.github.com>
2025-08-09 09:49:50 +0530

    Add files via upload
    This is a Weather Website which is used to see weather of any city that you want . 
    Technologies used: HTML , CSS , JavaScript and Weather APIs.

---
+++ weather.js
@@ -0,0 +1,36 @@
+  const apiKey ="bd6d2747ca089a2ae2fdde5466aa5845";
+    const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+    const searchBox = document.querySelector(".search input");
+    const searchBtn = document.querySelector(".search button");
+    const weatherIcon=document.querySelector(".weather-icon");
+    
+
+    async function checkWeather(city){
+        const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+        var data = await response.json();
+
+        console.log(data);
+
+        document.querySelector(".city").innerHTML = data.name;
+        document.querySelector(".temp").innerHTML =  Math.round(data.main.temp) + "°C";
+        document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+        document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+     if(data.weather[0].main=="Rain"){
+        weatherIcon.src="Images/rain.png";
+     }
+     else if(data.weather[0].main=="Clear"){
+        weatherIcon.src="Images/clear.png";
+     }
+     else if(data.weather[0].main=="Clouds"){
+        weatherIcon.src="Images/clouds.png";
+     }
+     else if(data.weather[0].main=="Drizzle"){
+        weatherIcon.src="Images/Drizzle.png";
+     }
+
+    }
+
+    searchBtn.addEventListener("click",()=>{
+        checkWeather(searchBox.value);
+    })
